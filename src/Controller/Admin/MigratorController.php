f4385c5f38438ae3bdb333f0d5cc4ec7dc7b92ee
ryuring <egashira@catchup.co.jp>
2023-11-25 19:41:26 +0900

    表示を調整

--- src/Controller/Admin/MigratorController.php
+++ src/Controller/Admin/MigratorController.php
@@ -2,21 +2,40 @@
 
 namespace BcDbMigrator\Controller\Admin;
 
+use BaserCore\Utility\BcUtil;
+
 /**
  * MigrationController
  */
 class MigratorController extends \BaserCore\Controller\Admin\BcAdminAppController
 {
+
+    /**
+     * beforeFilter
+     */
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+        $this->_tmpPath = TMP . 'dbmigrator' . DS;
+        $this->migrator = 'BcDbMigrator' . $this->getMajorVersion();
+        $migratorClass = $this->migrator . 'Component';
+        if (class_exists($migratorClass)) {
+            $this->loadComponent('BcDbMigrator.' . $this->migrator);
+        } else {
+            $this->BcMessage->setWarning('このプラグインは、このバージョンのbaserCMSに対応していません。');
+        }
+    }
+    
     /**
      * マイグレーション画面 
      */
     public function index()
     {
         $this->setTitle('baserCMS DBマイグレーター');
-        if ($this->getRequest()->getData()) {
+        if ($this->getRequest()->is('post')) {
             if ($this->_migrate($this->getRequest()->getData())) {
-                $version = str_replace(' ', '_', $this->getBaserVersion());
-                $this->Session->write('BcDbMigrator.file', 'baserbackup_' . $version . '_' . date('Ymd_His'));
+                $version = str_replace(' ', '_', BcUtil::getVersion());
+                $this->getRequest()->getSession()->write('BcDbMigrator.file', 'baserbackup_' . $version . '_' . date('Ymd_His'));
                 $this->BcMessage->setInfo('バックアップデータのマイグレーションが完了しました。ダウンロードボタンよりダウンロードしてください。');
                 $this->redirect('index');
             } else {
@@ -24,13 +43,18 @@ class MigratorController extends \BaserCore\Controller\Admin\BcAdminAppControlle
                 $this->redirect('index');
             }
         }
-        if ($this->Session->read('BcDbMigrator.downloaded')) {
-            $this->Session->delete('BcDbMigrator.file');
-            $this->Session->delete('BcDbMigrator.downloaded');
+        if ($this->getRequest()->getSession()->read('BcDbMigrator.downloaded')) {
+            $this->getRequest()->getSession()->delete('BcDbMigrator.file');
+            $this->getRequest()->getSession()->delete('BcDbMigrator.downloaded');
             $Folder = new \Cake\Filesystem\Folder($this->_tmpPath);
             $Folder->delete();
         }
-        $message = $this->{$this->migrator}->getMessage();
+        
+        if(isset($this->{$this->migrator})) {
+			$message = $this->{$this->migrator}->getMessage();
+		} else {
+			$message = [];
+		}
         if (!empty($message[0])) {
             $this->set('noticeMessage', $message);
         }
@@ -41,7 +65,7 @@ class MigratorController extends \BaserCore\Controller\Admin\BcAdminAppControlle
     public function download()
     {
         $this->autoRender = false;
-        $fileName = $this->Session->read('BcDbMigrator.file');
+        $fileName = $this->getRequest()->getSession()->read('BcDbMigrator.file');
         if (!$fileName || !is_dir($this->_tmpPath)) {
             $this->notFound();
         }
@@ -52,6 +76,91 @@ class MigratorController extends \BaserCore\Controller\Admin\BcAdminAppControlle
         $Simplezip->download($fileName);
         $Folder = new \Cake\Filesystem\Folder();
         $Folder->delete($this->_tmpPath);
-        $this->Session->write('BcDbMigrator.downloaded', true);
+        $this->getRequest()->getSession()->write('BcDbMigrator.downloaded', true);
     }
+	
+    /**
+     * マイグレーション実行
+     * 
+     * @param array $data リクエストデータ
+     * @return bool
+     */
+    protected function _migrate($data)
+    {
+        if (empty($data['Migrator']['backup']['tmp_name'])) {
+            return false;
+        }
+        // アップロードファイルを一時フォルダに解凍
+        if (!$this->_unzipUploadFileToTmp($data)) {
+            return false;
+        }
+        if (!$this->{$this->migrator}->migrate($data['Migrator']['encoding'])) {
+            return false;
+        }
+        return true;
+    }
+    
+    /**
+     * アップロードしたファイルを一時フォルダに解凍する
+     *
+     * @param array $data リクエストデータ
+     * @return bool
+     */
+    public function _unzipUploadFileToTmp($data)
+    {
+        $Folder = new \Cake\Filesystem\Folder();
+        $Folder->delete($this->_tmpPath);
+        $Folder->create($this->_tmpPath, 0777);
+        $targetPath = $this->_tmpPath . $data['Migrator']['backup']['name'];
+        if (!move_uploaded_file($data['Migrator']['backup']['tmp_name'], $targetPath)) {
+            return false;
+        }
+        // ZIPファイルを解凍する
+        $BcZip = new \BaserCore\Utility\BcZip();
+        if (!$BcZip->extract($targetPath, $this->_tmpPath)) {
+            return false;
+        }
+        //		$Simplezip = new Simplezip();
+        //		if (!$Simplezip->unzip($targetPath, $this->_tmpPath)) {
+        //			return false;
+        //		}
+        $Folder = new \Cake\Filesystem\Folder($this->_tmpPath);
+        $files = $Folder->read();
+        if (empty($files[0])) {
+            return false;
+        }
+        $valid = false;
+        $directFolder = '';
+        foreach ($files[0] as $file) {
+            if ($file === 'plugin') {
+                $valid = true;
+            }
+            $directFolder = $file;
+        }
+        if (!$valid) {
+            $Folder = new \Cake\Filesystem\Folder($this->_tmpPath . DS . $directFolder);
+            $files = $Folder->read();
+            if (empty($files[0])) {
+                return false;
+            }
+            foreach ($files[0] as $file) {
+                $Folder = new \Cake\Filesystem\Folder();
+                $Folder->move(['from' => $this->_tmpPath . DS . $directFolder . DS . $file, 'to' => $this->_tmpPath . DS . $file, 'chmod' => 777]);
+            }
+            $Folder->delete($this->_tmpPath . DS . $directFolder);
+        }
+        @unlink($targetPath);
+        return true;
+    }
+    
+    /**
+     * baserCMSのメジャーバージョンを取得
+     *
+     * @return string
+     */
+    public function getMajorVersion()
+    {
+        return preg_replace('/([0-9])\\..+/', "\$1", \BaserCore\Utility\BcUtil::getVersion());
+    }
+    
 }
--- templates/Admin/Migrator/index.php
+++ templates/Admin/Migrator/index.php
@@ -1,44 +1,64 @@
 <script>
-$(function(){
-	$("#BtnUpload").click(function(){
-		$.bcUtil.showLoader();
+	$(function () {
+		$("#BtnUpload").click(function () {
+			$.bcUtil.showLoader();
+		});
 	});
-});
 </script>
 
-<?php if(!empty($noticeMessage[0])): ?>
-	<div class="panel-box">
-		<h2>注意事項</h2>
-		<ul>
-			<li><?php echo implode('</li><li>', $noticeMessage) ?></li>
-		</ul>
+<section class="bca-section">
+	<?php if (!empty($noticeMessage[0])): ?>
+			<div class="panel-box">
+				<h2>注意事項</h2>
+				<ul>
+					<li><?php echo implode('</li><li>', $noticeMessage) ?></li>
+				</ul>
+			</div>
+	<?php endif ?>
+</section>
+
+<section class="bca-section">
+	<?php echo $this->BcAdminForm->create(null, ['type' => 'file']) ?>
+	
+	<table class="bca-form-table" id="ListTable">
+		<tr>
+			<th class="bca-form-table__label">
+		  <?php echo $this->BcAdminForm->label('encoding', '文字コード') ?>
+			</th>
+			<td class="bca-form-table__input">
+		  <?php echo $this->BcAdminForm->control('encoding', ['type' => 'radio', 'options' => ['auto' => '自動判別', 'UTF-8' => 'UTF-8', 'SJIS-win' => 'SJIS'], 'value' => 'auto']) ?>
+		  <?php echo $this->BcAdminForm->error('encoding') ?>
+			</td>
+		</tr>
+		<tr>
+			<th class="bca-form-table__label">
+		  <?php echo $this->BcAdminForm->label('backup', 'バックアップファイル') ?>
+			</th>
+			<td class="bca-form-table__input">
+		  <?php echo $this->BcAdminForm->control('backup', ['type' => 'file']) ?>
+		  <?php echo $this->BcAdminForm->error('backup') ?>
+			</td>
+		</tr>
+	</table>
+</section>
+
+<section class="bca-actions">
+	<div class="bca-actions__main">
+	  <?php echo $this->BcAdminForm->submit('アップロード', [
+	  	'div' => false, 
+	  	'class' => 'bca-btn bca-actions__item', 
+	  	'id' => 'BtnUpload',
+	  	'data-bca-btn-type' => "save",
+	  	'data-bca-btn-size' => "lg",
+	  	'data-bca-btn-width' => "lg"
+		]) ?>
 	</div>
-<?php endif ?>
+	<div class="bca-actions__sub">
+	  <?php if ($this->getRequest()->getSession()->read('BcDbMigrator.file')): ?>
+				　<?php echo $this->BcBaser->link('ダウンロード', ['action' => 'download'], ['class' => 'bca-btn']) ?>
+	  <?php endif ?>
+	</div>
+</section>
 
-<?php echo $this->BcAdminForm->create('Migrator', array('type' => 'file')) ?>
-
-<table cellpadding="0" cellspacing="0" class="list-table" id="ListTable">
-	<tr>
-		<th class="col-head"><span class="required">*</span>&nbsp;<?php echo $this->BcAdminForm->label('Migrator.encoding', '文字コード') ?></th>
-		<td class="col-input">
-			<?php echo $this->BcAdminForm->control('Migrator.encoding', array('type' => 'radio', 'options' => array('auto' => '自動判別', 'UTF-8' => 'UTF-8', 'SJIS-win' => 'SJIS'), 'value' => 'auto')) ?>
-			<?php echo $this->BcAdminForm->error('Migrator.encoding') ?>
-		</td>
-	</tr>
-	<tr>
-		<th class="col-head"><span class="required">*</span>&nbsp;<?php echo $this->BcAdminForm->label('Migrator.backup', 'バックアップファイル') ?></th>
-		<td class="col-input">
-			<?php echo $this->BcAdminForm->control('Migrator.backup', array('type' => 'file')) ?>
-			<?php echo $this->BcAdminForm->error('Migrator.backup') ?>
-		</td>
-	</tr>
-</table>
-
-<div class="submit">
-	<?php echo $this->BcAdminForm->submit('アップロード', array('div' => false, 'class' => 'button', 'id' => 'BtnUpload')) ?>
-<?php if($this->Session->read('BcDbMigrator.file')): ?>
-	　<?php echo $this->BcBaser->link('ダウンロード', array('action' => 'download'), array('class' => 'button')) ?>
-<?php endif ?>
-</div>
 
 <?php echo $this->BcAdminForm->end() ?>
